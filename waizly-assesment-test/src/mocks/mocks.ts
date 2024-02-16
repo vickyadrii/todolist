4e01849589923a75e539982777d7287035b6f490
vickyadri29 <vickyadri290501@gmail.com>
2024-02-16 17:28:57 +0800

    feat: config tailwind + shadcn-ui

---
+++ waizly-assesment-test/src/mocks/mocks.ts
@@ -0,0 +1,23 @@
+interface Students {
+  id: number;
+  name: string;
+  age: number;
+}
+
+export const students: Students[] = [
+  {
+    id: 1,
+    name: "Vicky",
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "Adri",
+    age: 20,
+  },
+  {
+    id: 3,
+    name: "Herd",
+    age: 23,
+  },
+];
