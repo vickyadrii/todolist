96f72e0ced2426dede768d70d916db56fc637dda
vickyadri29 <vickyadri290501@gmail.com>
2024-02-16 14:58:04 +0800

    feat: added problem-solving-3 folder

---
+++ problem-solving-3/index.js
@@ -0,0 +1,30 @@
+// Problem Solving Basic 3
+
+const convertTo24HourFormat = (time) => {
+  const [timePart, meridiem] = time.split(/(?=[AP]M)/);
+
+  let [hours, minutes, seconds] = timePart.split(":");
+  
+  hours = parseInt(hours);
+
+  if (meridiem === "PM" && hours !== 12) {
+    hours += 12;
+  } else if (meridiem === "AM" && hours === 12) {
+    hours = 0;
+  }
+
+  if (hours > 24 && meridiem === 'PM') {
+    return 'Invalid Date'
+  } else if (hours > 12 && meridiem === 'AM') {
+    return 'Invalid Date'
+  }
+
+  console.log({hours, meridiem})
+
+  return `${hours.toString().padStart(2, "0")}:${minutes}:${seconds}`;
+}
+
+const time = "12:01:00AM";
+const convertedTime = convertTo24HourFormat(time);
+
+console.log(convertedTime);
