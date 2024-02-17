caacb9619e1c9c4eab11a0cfa189ffae2ff290be
vickyadri29 <vickyadri290501@gmail.com>
2024-02-17 20:19:46 +0800

    fix: fixed issue deployment the app

--- waizly-assesment-test/src/app/layout.tsx
+++ waizly-assesment-test/src/app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from 'next';
+import { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 
-export const fontSans = FontSans({
+// Call the FontSans function and assign it to a const at the module scope
+const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans'
 });
--- waizly-assesment-test/src/app/page.tsx
+++ waizly-assesment-test/src/app/page.tsx
@@ -2,11 +2,11 @@ import HomePage from '@/features/home/HomePage';
 import { Weather } from '@/types/types';
 
 
-export async function getWeatherApi() {
+async function getWeatherApi() {
   const res = await fetch(
     'https://api.openweathermap.org/data/2.5/weather?q=Palu&units=metric&appid=800fd005579d27a234b5ea3722aeb935'
   );
-  const data = await res.json();
+  const data = res.json();
   return data
 }
 
